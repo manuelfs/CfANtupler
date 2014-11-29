db8a2c8deb889bd2a4ffcef4cb2ffc0d9d495682
Manuel Franco Sevilla <manuelf@physics.ucsb.edu>
2014-11-29 05:17:30 +0100

    Updated the CRAB cfg file with new conf variables and automatic naming of job

--- minicfa/python/crabcfA.py
+++ minicfa/python/crabcfA.py
@@ -1,8 +1,18 @@
+## Configuration file to CRAB3 cfA jobs
+## Submit for src with: crab submit -c crabcfA.py
+
+
+dataset = '/SMS-T2tt_2J_mStop-850_mLSP-100_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
+
+jobname = dataset[1:].replace('/','__')
+jobname = jobname.replace(':','___')
+
+
 from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'T1bbbb'
+config.General.requestName = jobname
 config.General.workArea = 'out_crab'
 
 config.section_("JobType")
@@ -10,13 +20,12 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'CfANtupler/minicfa/python/minicfA_cfg.py'
 
 config.section_("Data")
-config.Data.inputDataset = '/SMS-T1bbbb_2J_mGl-1000_mLSP-900_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM'
-config.Data.dbsUrl = 'global'
+config.Data.inputDataset = dataset
+config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 2
+config.Data.unitsPerJob = 1
 config.Data.publication = True
-config.Data.publishDbsUrl = 'phys03'
-config.Data.publishDataName = 'T1bbbb_cfA'
+config.Data.publishDBS = 'phys03'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_US_UCSD'
